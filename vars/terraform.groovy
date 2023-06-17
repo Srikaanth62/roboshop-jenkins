5610a32bf3a7164f6c0f1199ad5268550a1ec2f9
srikaanth <srikaanth62@gmail.com>
2023-06-17 11:35:08 +0530

    jenkins groovy file

---
+++ vars/terraform.groovy
@@ -0,0 +1,35 @@
+def call() {
+    pipeline {
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+        }
+        stages {
+            stage('terraform INIT') {
+                steps {
+                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
+                }
+
+            }
+            stage('terraform APPLY') {
+                steps {
+                    sh 'terraform apply -auto-approve -var-file=env-${env}/main.tfvars'
+                }
+            }
+
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+    }
+
+
+
